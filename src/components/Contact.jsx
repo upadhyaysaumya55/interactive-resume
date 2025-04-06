505c794c26b17454895a1be9709e77a96999b9f4
upadhyaysaumya55 <saumyaupadhyay621@gmail.com>
2025-04-06 19:30:18 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
         <section id="contact" className="contact">
             <h2>Contact Me</h2>
             <p>Email: saumyaupadhyay621@gmail.com</p>
-            <p>Phone: +91 6205745284</p>
+            <p>Phone: +91 XXXXXXXXXX</p>
         </section>
     );
 };
